8dfbac0c9654fda1c040f29ae53edb19b7f152ac
Amarjit Kumar Natraj <143289868+kumaramar5526@users.noreply.github.com>
2023-10-27 09:53:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function convertToCelsius() {
+    const fahrenheit = parseFloat(document.getElementById("fahrenheit").value);
+    if (!isNaN(fahrenheit)) {
+        const celsius = (fahrenheit - 32) * (5 / 9);
+        document.getElementById("celsius").value = celsius.toFixed(2);
+    } else {
+        alert("Please enter a valid temperature in Fahrenheit.");
+    }
+}
+
+function convertToFahrenheit() {
+    const celsius = parseFloat(document.getElementById("celsius").value);
+    if (!isNaN(celsius)) {
+        const fahrenheit = (celsius * (9 / 5)) + 32;
+        document.getElementById("fahrenheit").value = fahrenheit.toFixed(2);
+    } else {
+        alert("Please enter a valid temperature in Celsius.");
+    }
+}
